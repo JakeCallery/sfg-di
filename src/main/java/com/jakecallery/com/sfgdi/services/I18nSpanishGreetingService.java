ca95d19c5675d25c47394e81a24d2fa12e3ad189
Jake <jake@subvoicestudios.com>
2021-06-23 21:17:16 -0400

    added default profile

--- src/main/java/com/jakecallery/com/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/com/jakecallery/com/sfgdi/services/I18nSpanishGreetingService.java
@@ -3,7 +3,8 @@ package com.jakecallery.com.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
+//@Profile({"ES"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
