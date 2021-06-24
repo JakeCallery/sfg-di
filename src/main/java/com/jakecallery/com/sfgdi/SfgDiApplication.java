09ecccd166da291df341c6f68361d7aef7f69956
Jake <jake@subvoicestudios.com>
2021-06-23 20:58:35 -0400

    practice with the @primary annotation. Updated MyController to use autowire

--- src/main/java/com/jakecallery/com/sfgdi/SfgDiApplication.java
+++ src/main/java/com/jakecallery/com/sfgdi/SfgDiApplication.java
@@ -16,10 +16,10 @@ public class SfgDiApplication {
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
-		String greeting = myController.sayHello();
-		System.out.println(greeting);
+		System.out.println("------ Primary Bean");
+		System.out.println(myController.sayHello());
 
-		System.out.println("------ Property");
+		System.out.println("\n------ Property");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
 
