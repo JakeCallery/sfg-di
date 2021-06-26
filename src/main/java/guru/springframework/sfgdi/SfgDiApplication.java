e774a43fb6d2dce83a58a1bfb0f07d8531656e59
Jake <jake@subvoicestudios.com>
2021-06-25 20:43:12 -0400

    xml config example for one bean

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 //@ComponentScan(basePackages = {"guru.springframework.sfgdi", "com.springframework.pets"})
 @SpringBootApplication
+@ImportResource("classpath:sfgdi-config.xml")
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
